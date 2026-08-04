37b2aaacd49c7b634f955651e8fcaa8cf91e83e6
Da Kexing <dakexing@food-showcase>
2026-08-04 16:00:05 +0800

    chore: 移除死代码 config.js 和 migrate-images-to-supabase.js

--- js/config.js
+++
@@ -1,9 +0,0 @@
-// Supabase 配置
-var SUPABASE_URL = 'https://rihlfwgrqdxgygyvatda.supabase.co';
-var SUPABASE_ANON_KEY = '[YOUR_SUPABASE_ANON_KEY]';
-
-// 腾讯云 COS 配置
-var COS_BUCKET = 'foodshowcase-1308216845';
-var COS_REGION = 'ap-guangzhou';
-var COS_SECRET_ID = '[YOUR_COS_SECRET_ID]';
-var COS_SECRET_KEY = '[YOUR_COS_SECRET_KEY]';
